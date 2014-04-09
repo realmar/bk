17cdea43c981e643553c9fd7179c1a8bba0f3aca
Anastassios Martakos <amartako@phys.ethz.ch>
2014-04-09 15:47:22 +0200

    Fixed ERR MSG handling

--- public/javascript/scripts/prototypes/ActionHandler.js
+++ public/javascript/scripts/prototypes/ActionHandler.js
@@ -4,7 +4,7 @@ function ActionHandler(msg_data_arg) {
     this.ProcessAction = ProcessAction;
 
     function ProcessAction() {
-        RemoveMessageData($("." . AH_ERROR));
+        RemoveMessageData($("." + AH_ERROR));
         if(this.msg_data["all_errors"].length <= 0) {
             programm_handler.CheckBookboxStates();
             for(var i = 0; i < this.msg_data["msg_data"].length; i++) {
@@ -16,8 +16,9 @@ function ActionHandler(msg_data_arg) {
             RemoveMessageData($("div#msg_user_client > p." + CHAG_CONT));
             programm_handler.last_data_state = this.msg_data["msg_data"];
         }else{
-            for(var err_data in this.msg_data["all_errors"]) {
-                AddMessageData($("div#msg_errors"), "<p class=" + MSG_ERRORS + " " + AH_ERROR + "ERROR: err_type: " + err_data["err_type"] + " err_string: " + err_data["error_msg"] + "</p>");
+            for(var i = 0; i < this.msg_data["all_errors"].length; i++) {
+                AddMessageData($("div#msg_errors"), '<p class="' + MSG_ERRORS + ' ' + AH_ERROR + '">ERROR: err_type: ' + this.msg_data["all_errors"][i]["error_type"] + ' err_string: ' + this.msg_data["all_errors"][i]["error_msg"] + '</p>');
+                $("div#msg_errors").removeClass("display_none");
             }
         }
         HideLoadingMessage();
