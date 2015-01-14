19b3ca0c265d13e9981e9ff1f2236d22ec8e0913
Anastassios Martakos <amartako@phys.ethz.ch>
2015-01-14 14:08:13 +0100

    Removed unnessessaire opendoor entry in DB

--- config/DBConfig.sql
+++ config/DBConfig.sql
@@ -2,18 +2,17 @@ DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users (
     doornumber INTEGER PRIMARY KEY,
-    username   TEXT,
-    opendoor   INTEGER
+    username   TEXT
 );
 
-INSERT INTO Users(doornumber, username, opendoor) VALUES(0, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(1, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(2, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(3, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(4, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(5, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(6, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(7, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(8, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(9, NULL, 0);
-INSERT INTO Users(doornumber, username, opendoor) VALUES(10, NULL, 0);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(0, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(1, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(2, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(3, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(4, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(5, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(6, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(7, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(8, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(9, NULL);
+INSERT INTO Users(doornumber, username, opendoor) VALUES(10, NULL);
