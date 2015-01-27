898764e618266acb186d2227f3219329bfdc8a6e
Anastassios Martakos <amartako@phys.ethz.ch>
2015-01-27 10:49:12 +0100

    create db correction in cmd in install sh

--- config/InitializeProgram.sh
+++ config/InitializeProgram.sh
@@ -129,7 +129,7 @@ if [[ $INSTBK =~ ^(yes|y) ]] || [[ -z $INSTBK ]]; then
     echo 'Setting up the database'
     echo 'AFTER DATABASE IS SET UP EXIT THE SQLITE3 CONSOLE WITH .exit TO CONTINUE THE SETUP'
 
-    sqlite3 -init $PA/config/DBConfig.sql $PA/config/database/BKDatabase.db
+    sqlite3 -init $PA/config/DBConfig.sql $PA/database/BKDatabase.db
 
     echo ''
     echo ''
