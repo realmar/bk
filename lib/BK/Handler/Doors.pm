bc44822909db9a301f8cc28ad656fff0a158be50
Anastassios Martakos <amartako@phys.ethz.ch>
2015-01-28 17:22:23 +0100

    Optimized door code

--- lib/BK/Handler/Doors.pm
+++ lib/BK/Handler/Doors.pm
@@ -36,20 +36,20 @@ sub OpenDoor {
 
     if($doors_opened_err > 0) {
         $self->SUPER::ThrowMessage(Constants::ERROR, Constants::DOORSEXEPTIONOPENEND, MessagesTextConstants::DOORSERRORCODE . $doors_opened_err);
-    }else{
-        $self->SUPER::ThrowMessage(Constants::LOG, Constants::DOOROPENED, MessagesTextConstants::DOORSNUMBER . $door . MessagesTextConstants::DOORSUSERNAME . $username);
-        sleep(Constants::DOORSSENDSIGNALTIME);
+        return 1;
     }
+    $self->SUPER::ThrowMessage(Constants::LOG, Constants::DOOROPENED, MessagesTextConstants::DOORSNUMBER . $door . MessagesTextConstants::DOORSUSERNAME . $username);
+    sleep(Constants::DOORSSENDSIGNALTIME);
 
     my $doors_closed_err = CommonVariables::SetPins(Constants::HEXTWOBYTEONE, Constants::HEXNULL, Constants::HEXNULL, Constants::HEXNULL);
 
     if($doors_closed_err > 0) {
         $self->SUPER::ThrowMessage(Constants::ERROR, Constants::DOORSEXEPTIONCLOSED, MessagesTextConstants::DOORSERRORCODE . $doors_closed_err);
-    }else{
-        $self->SUPER::ThrowMessage(Constants::LOG, Constants::DOORCLOSED, MessagesTextConstants::DOORSNUMBER . $door . MessagesTextConstants::DOORSUSERNAME . $username);
+        return 1;
     }
+    $self->SUPER::ThrowMessage(Constants::LOG, Constants::DOORCLOSED, MessagesTextConstants::DOORSNUMBER . $door . MessagesTextConstants::DOORSUSERNAME . $username);
 
-    return $doors_opened_err > 0 || $doors_closed_err > 0 ? undef : 2;
+    return undef;
 }
 
 1;
